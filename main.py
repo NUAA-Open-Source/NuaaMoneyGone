e8ab77d9925f4ca9cc776f29b44ae0195e99cfcb
冰耀 <wongguobin@gmail.com>
2018-04-04 16:11:15 +0800

    finish sdk

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
+
+__author__ = 'arcosx'
+
 import requests
-import sys
 
 
 class client(object):
@@ -14,7 +16,6 @@ class client(object):
         'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1',
         'Content-Type': 'application/x-www-form-urlencoded',
         'Referer': 'https://app.nuaa.edu.cn/cardhis/wap/default/index',
-        'Cookie': ''
     }
 
     def __init__(self, username, password):
@@ -23,17 +24,13 @@ class client(object):
 
     def login(self):
         post_data = {'username': self.username, 'password': self.password}
-        login_res = requests.post(self.login_url, data=post_data)
-        if login_res.json()['e'] == '10011':
-            print(login_res.json()['m'])
-            return 0
-
+        login_res = requests.post(self.login_url, data=post_data, headers=self.headers)
         cookies = login_res.cookies
         UUkey = 'UUkey=' + cookies['UUkey'] + '; '
         vjuid = 'vjuid=' + cookies['vjuid'] + '; '
-        # vjvd = 'vjvd=' + cookies['vjvd'] + '; '
+        vjvd = 'vjvd=' + cookies['vjvd'] + '; '
         vt = 'vt=' + cookies['vt']
-        header_cookie = UUkey + vjuid + 'vjvd=03a083d8839b3be4c35122faf82a165c; ' + vt
+        header_cookie = UUkey + vjuid + vjvd + vt
         self.headers['Cookie'] = header_cookie
         return 1
 
@@ -42,13 +39,9 @@ class client(object):
             return
         payload = "sdate=" + begin_data + "&edate=" + end_data
         res = requests.post(self.search_url, headers=self.headers, data=payload)
-        print(res.json())
+        return res.json()
 
 
 if __name__ == '__main__':
-    username = sys.argv[1]
-    password = sys.argv[2]
-    begin_data = sys.argv[3]
-    end_data = sys.argv[4]
-    test = client(username=username, password=password)
-    test.search(begin_data=begin_data, end_data=end_data)
+    test = client(username='', password='')
+    print(test.search(begin_data='2015-06-06', end_data='2018-04-03'))
